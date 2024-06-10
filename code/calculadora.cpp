099ba7cc74ecabbaad203dd4dfad9932367be642
Richard Muler <richard.amaral.rj21@gmail.com>
2024-06-10 15:43:35 -0300

    commit 1.0
    Inclusão da biblioteca "string.h" e laço de repetição do tipo WHILE

--- code/calculadora.cpp
+++ code/calculadora.cpp
@@ -1,22 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main () {
 	//variáveis
 	float sal,porcentagem,salAumento;	
+	char opcao[30] = "SIM";
+
+	//laço de rep
+	while(strcmp(opcao, "SIM") == 0) {
 	
-	//entrada de dados
-	printf("Informe o salario do funcionario:\n");
-	scanf("%f",&sal);
-	printf("Informe o percentual de aumento desse funcionario:\n");
-	scanf("%f",&porcentagem);
-	//atribuição
-	salAumento = (sal * (porcentagem / 100) ) + sal; 
+		//entrada de dados
+		printf("Informe o salario do funcionario:\n");
+		scanf("%f",&sal);
+		printf("Informe o percentual de aumento desse funcionario:\n");
+		scanf("%f",&porcentagem);
+		//atribuição
+		salAumento = (sal * (porcentagem / 100) ) + sal; 
+		
+		//saída de dados
+		printf("----------------------------\n");
+		printf("Salario reajustado: %.1f\n",salAumento);
 	
-	//saída de dados
-	printf("----------------------------\n");
-	printf("Salario reajustado: %.1f\n",salAumento);
-	
+	//verificando o desejo do usuário
+	printf("Deseja continuar? Escreva em maiusculo\n");
+	scanf("%s",&opcao);
+
+	//limpar console
+	system("cls");
+	}
+
 	system("pause");
 	return 0;
 }
